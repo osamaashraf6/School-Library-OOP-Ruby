b6e32718c0739f40a8b5d0b7ca3261715ebe7758
osamaashraf6 <osamaashraf668@gmail.com>
2023-02-02 20:30:31 +0200

    Modify the project with some classes

---
+++ book.rb
@@ -0,0 +1,13 @@
+class Book
+  attr_accessor :title, :author, :rentals
+
+  def initialize(title, author)
+    @title = title
+    @author = author
+    @rentals = []
+  end
+
+  def add_rental(person, date)
+    @rentals.push(person, date, self)
+  end
+end
--- trimmer_decorator.rb
+++
@@ -1,6 +0,0 @@
-require './decorator'
-class TrimmerDecorator < Decorator
-  def correct_name
-    @nameable.correct_name.strip[0..9]
-  end
-end
