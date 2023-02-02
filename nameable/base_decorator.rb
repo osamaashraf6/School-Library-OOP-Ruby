eef30ccf31e3721fcd7403f011f17dbc35ae359b
osamaashraf6 <osamaashraf668@gmail.com>
2023-02-02 20:29:52 +0200

    Add the nameable folder for capitalize and trim for the name

---
+++ nameable/base_decorator.rb
@@ -0,0 +1,14 @@
+require_relative 'nameable_decorator'
+
+class NameableDecorator < Nameable
+  attr_accessor :nameable
+
+  def initialize(nameable)
+    super()
+    @nameable = nameable
+  end
+
+  def correct_name
+    @nameable.correct_name
+  end
+end
