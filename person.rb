524cd5aa89abe13a167fdacc711be5c92163501b
osamaashraf6 <osamaashraf668@gmail.com>
2023-02-02 19:04:46 +0200

    Update the class person

--- person.rb
+++ person.rb
@@ -1,9 +1,12 @@
-class Person
+require_relative 'nameable'
+
+class Person < Nameable
   attr_accessor :name, :age
   attr_reader :id
 
-  def initialize(age, name = 'Unknown', parent_permission = true)
-    @id = rand(1_000_000)
+  def initialize(age, name: 'Unknown', parent_permission: true)
+    super()
+    @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
@@ -11,6 +14,10 @@ class Person
 
   def can_use_services?
     is_of_age? || @parent_permission
+  end
+
+  def correct_name
+    @name
   end
 
   private
