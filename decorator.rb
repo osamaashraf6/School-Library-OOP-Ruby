cb90d094dfc159a0c334a69486396e8a30b5d0c9
osamaashraf6 <osamaashraf668@gmail.com>
2023-02-02 19:05:51 +0200

    Add the decrator class for the project

---
+++ decorator.rb
@@ -0,0 +1,11 @@
+require_relative './nameable'
+class Decorator < Nameable
+  def initialize(nameable)
+    @nameable = nameable
+    super()
+  end
+
+  def correct_name
+    @nameable.correct_name
+  end
+end
